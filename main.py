5d8bb1e10fe8e97a3c7559f05f2318e3c9777dfe
number one cyber developer. <146389741+archescyber@users.noreply.github.com>
2024-10-05 10:40:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ os.system('clear')
 
 L = instaloader.Instaloader()
 print("""
-  ___ ____          _                _                    
- |_ _/ ___|  _     / \   _ __   __ _| |_   _ _______ _ __ 
+  ___ ____          _                _
+ |_ _/ ___|  _     / \   _ __   __ _| |_   _ _______ _ __
   | | |  _  (_)   / _ \ | '_ \ / _` | | | | |_  / _ \ '__|
-  | | |_| |  _   / ___ \| | | | (_| | | |_| |/ /  __/ |   
- |___\____| (_) /_/   \_\_| |_|\__,_|_|\__, /___\___|_|   
-                                       |___/              
+  | | |_| |  _   / ___ \| | | | (_| | | |_| |/ /  __/ |
+ |___\____| (_) /_/   \_\_| |_|\__,_|_|\__, /___\___|_|
+                                       |___/
 
-                                   """)                       
+                                   """)
 print("[•] The processing time is approximately 1 or 2 minutes.")
 print("")
 print("[•] Too many consecutive attempts may trigger Instagram API limits.")
@@ -21,19 +21,25 @@ username = input("[•] Instagram Username: ")
 print("")
 password = input("[•] Instagram Password: ")
 
-
 L.login(username, password)
-
 
 profile = instaloader.Profile.from_username(L.context, username)
 following = set(profile.get_followees())
-
-
 followers = set(profile.get_followers())
-
 
 not_following_back = following - followers
 
+# Dosya adı oluşturuluyor
+filename = f"{username}-analyze.txt"
+
+# Verileri dosyaya yazma işlemi
+with open(filename, "w") as file:
+    file.write("Users Who Do Not Follow You Back:\n")
+    file.write("-" * 30 + "\n")
+    for idx, user in enumerate(not_following_back, start=1):
+        file.write(f"{idx}. {user.username}\n")
+    file.write("-" * 30 + "\n")
+    file.write(f"Total: {len(not_following_back)} user.\n")
 
 print("\n[•] Users Who Do Not Follow You:")
 print("-" * 30)
@@ -43,3 +49,4 @@ for idx, user in enumerate(not_following_back, start=1):
 
 print("-" * 30)
 print(f"Total: {len(not_following_back)} user.")
+print(f"\n[•] The data has been saved in {filename}.")
